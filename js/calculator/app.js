8e20862d32ccf44550611b5d7bac558204a39958
Omer Dolev <omerdolev90@gmail.com>
2023-05-07 18:02:30 +0300

    class-23

--- js/calculator/app.js
+++ js/calculator/app.js
@@ -1,10 +1,19 @@
+const display = document.getElementById("screen");
 const numbersContainer = document.getElementById("numbers-container");
 const numberButtons = numbersContainer.getElementsByTagName("button");
 
 for (let i = 0; i < numberButtons.length; i++) {
-    numberButtons[i].addEventListener("click", sayHello);
+    numberButtons[i].addEventListener("click", onNumberButtonClick);
 }
 
-function sayHello() {
-    alert("hello");
+let currentNumber = 0;
+let total = 0;
+
+display.innerText = currentNumber.toString();
+
+function onNumberButtonClick(e) {
+    const num = Number(e.target.getAttribute("data-value"));
+    
+    currentNumber = (currentNumber * 10) + num;
+    display.innerText = currentNumber.toString();
 }
